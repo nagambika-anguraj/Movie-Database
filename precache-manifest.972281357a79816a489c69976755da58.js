dc2a9a745fbe9953c09ffe268cd1f9feadafd0ac
unknown <nagambika.anguraj@gmail.com>
2020-07-06 09:51:16 +0530

    Updates

---
+++ precache-manifest.972281357a79816a489c69976755da58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d60b6a0354cf175b85d9b6e82efdabc",
+    "url": "/Movie-Database/index.html"
+  },
+  {
+    "revision": "9819dce45ad6ffdee1f8",
+    "url": "/Movie-Database/static/css/main.e1434828.chunk.css"
+  },
+  {
+    "revision": "120fb1e11efd68ad1953",
+    "url": "/Movie-Database/static/js/2.f1912f7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Database/static/js/2.f1912f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9819dce45ad6ffdee1f8",
+    "url": "/Movie-Database/static/js/main.c1b56e01.chunk.js"
+  },
+  {
+    "revision": "1d4a7c3a66337f4267f8",
+    "url": "/Movie-Database/static/js/runtime-main.4d21f6ab.js"
+  },
+  {
+    "revision": "f362aeab7bccfbb6f6cd8fc7a894213a",
+    "url": "/Movie-Database/static/media/movie.f362aeab.png"
+  }
+]);
